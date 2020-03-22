ec78980ae094bcca1373cc8873cddc57358f54c8
Ybrayym <ybrayymjan90@gmail.com>
2020-03-21 21:38:13 -0400

    Passing everything

---
+++ numseq/__init__.py
@@ -0,0 +1,1 @@
+__author = """Ybrayym Abamov"""
---
+++ numseq/geo.py
@@ -0,0 +1,13 @@
+def square(n):
+    """ n is being squared """
+    return n*n
+
+
+def triangle(n):
+    """ function that finds the dimension of a triangular object """
+    return n * (n+1) / 2
+
+
+def cube(n):
+    """ n is being cubed """
+    return n**3
